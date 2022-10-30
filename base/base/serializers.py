ec143d0b5d52aa72b557a4dcd6f07e81d7ce3c26
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2022-10-31 00:35:38 +0200

    Delete base directory

--- base/base/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from base.models import Element
-
-class ElementSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Element
-        fields = '__all__'
